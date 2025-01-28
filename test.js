e9a84106450321c579ca475d7dd88952f9da64bc
radioliver <radi.oliver@diak.szbi-pg.hu>
2025-01-28 09:26:56 +0100

    buttons

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 console.log("Hello World!")
+console.log("Az elso JavaScript kód")
+function kep_beilleszt() { document.getElementById("kep_helye").innerHTML = "<img src='image.png'>"; }
+function kep_torol() { document.getElementById("kep_helye").innerHTML = ""; }
