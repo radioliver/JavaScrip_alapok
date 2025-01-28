953bca829f9b0594d34ecac4d2e94a86e0e88b0c
radioliver <radi.oliver@diak.szbi-pg.hu>
2025-01-28 09:46:36 +0100

    console types

---
+++ console_log_types.js
@@ -0,0 +1,10 @@
+console.log("Ez egy normál üzenet")
+console.warn("Ez egy figyelmeztetés")
+console.error("Ez egy hibaüzenet")
+console.table{
+    [
+        {name: "Szilveszter", age: 20},
+        {name: "kardi", age: 11},
+        {name: "fat", age: 10}
+    ]
+}
